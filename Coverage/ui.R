4d41eb2c24ffc35e2de98e39442e1e7cc84ccdb8
Sriram Ramesh <sramesh5@ucsc.edu>
2020-07-17 10:00:04 -0700

    Input UI (#1)
    Add UI for user input

--- Coverage/ui.R
+++ Coverage/ui.R
@@ -9,25 +9,24 @@
 
 library(shiny)
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
-
+shinyUI(pageWithSidebar(
+    
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
+    headerPanel("Search for database coverage of your species and barcodes of interest"),
+    
+    # Usage instructions
+    mainPanel(
+      p("Enter the names of your species of interest and genetic bar codes of interest
+         on separate lines")
+    ),
+    
+    # Sidebar with a text area for organisms and bar code
+    sidebarPanel(
+        textAreaInput(inputId = "organismList", label = "Species Names"),
+        textAreaInput(inputId = "barcodeList", label = "Barcodes of Interest"),
+        actionButton(inputId = "searchButton", label = "Search")
     )
+    
+    # Show a plot of the generated distribution
+    
 ))
