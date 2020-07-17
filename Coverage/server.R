98500b7beb4b0fe31754df1abba5c2464d064216
Sriram Ramesh <sramesh5@ucsc.edu>
2020-07-17 10:00:04 -0700

    Input UI (#1)
    Add UI for user input

--- Coverage/server.R
+++ Coverage/server.R
@@ -9,18 +9,32 @@
 
 library(shiny)
 
-# Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+    
+    output$allInputs <- renderUI({
+        # Get value of button, which represents number of times pressed (i.e. number of inputs added)
+        inputsToShow <- input$appendInput
+        # Return if button not pressed yet
+        if(is.null(inputsToShow) || inputsToShow < 1) return()
+        # Initialize list of inputs
+        inputTagList <- tagList()
+        # Populate the list of inputs
+        lapply(1:inputsToShow,function(i){
+            # Define unique input id and label
+            newInputId <- paste0("input", i)
+            newInputLabel <- paste("Input", i)
+            # Prevent dynamic inputs from resetting
+            newInputValue <- "Option 1"
+            if (newInputId %in% names(input)) {
+                newInputValue <- input[[newInputId]]
+            }
+            # Define new input
+            newInput <- textInput(newInputId, newInputLabel)
+            # Append new input to list of existing inputs
+            inputTagList <<- tagAppendChild(inputTagList, newInput)
+        })
+        # Return updated list of inputs
+        inputTagList
     })
-
+    
 })
