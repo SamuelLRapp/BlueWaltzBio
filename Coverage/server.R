8bccc119c5a44d5299ac22a13a32b1a6690ed9f9
Sriram Ramesh <sramesh5@ucsc.edu>
2020-07-18 16:23:16 -0700

    Output table (#3)
    Make the app output the results of the database queries to a table

--- Coverage/server.R
+++ Coverage/server.R
@@ -12,22 +12,35 @@ library(rentrez)
 
 shinyServer(function(input, output) {
     
-    
-    
-    output$coverageResults <- renderUI({
-        organismList <- strsplit(input$organismList, ",")
-        barcodeList <- strsplit(input$barcodeList, ",")
-        listLength <- length(organismList[[1]])
+    coverage <- reactive({
+        organismListLength <- length(organismList())
+        codeListLength <- length(barcodeList())
         searchTerm <- ""
         searchResult <- 0
-        for(organism in organismList[[1]]){
-            for(code in barcodeList[[1]]){
+        results <- c()
+        for(organism in organismList()){
+            for(code in barcodeList()){
                 searchTerm <- paste(organism, "[ORGN] AND ", code, "[GENE]", sep="")
                 searchResult <- entrez_search(db = "nucleotide", term = searchTerm)
+                results <- c(results, length(searchResult$ids))
             }
         }
-        numResults <- length(searchResult$ids)
-        toString(numResults)
+        data <- matrix(results, nrow = organismListLength, ncol = codeListLength, byrow = TRUE)
+        data
     })
+    
+    organismList <- reactive({
+        organismList <- strsplit(input$organismList, ",")
+        organismList[[1]]
+    })
+    
+    barcodeList <- reactive({
+        barcodeList <- strsplit(input$barcodeList, ",")
+        barcodeList[[1]]
+    })
+    
+    output$coverageResults <- DT::renderDataTable(
+        coverage(), rownames = organismList(), colnames = barcodeList()
+    )
     
 })
