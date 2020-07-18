cbcb56aecea600111ac150c92ec1ff24c08ab9f4
Sriram Ramesh <sramesh5@ucsc.edu>
2020-07-18 11:52:18 -0700

    Entrez query (#2)
    Make app capable of finding number of search results from entrez queries

--- Coverage/server.R
+++ Coverage/server.R
@@ -8,33 +8,26 @@
 #
 
 library(shiny)
+library(rentrez)
 
 shinyServer(function(input, output) {
     
-    output$allInputs <- renderUI({
-        # Get value of button, which represents number of times pressed (i.e. number of inputs added)
-        inputsToShow <- input$appendInput
-        # Return if button not pressed yet
-        if(is.null(inputsToShow) || inputsToShow < 1) return()
-        # Initialize list of inputs
-        inputTagList <- tagList()
-        # Populate the list of inputs
-        lapply(1:inputsToShow,function(i){
-            # Define unique input id and label
-            newInputId <- paste0("input", i)
-            newInputLabel <- paste("Input", i)
-            # Prevent dynamic inputs from resetting
-            newInputValue <- "Option 1"
-            if (newInputId %in% names(input)) {
-                newInputValue <- input[[newInputId]]
+    
+    
+    output$coverageResults <- renderUI({
+        organismList <- strsplit(input$organismList, ",")
+        barcodeList <- strsplit(input$barcodeList, ",")
+        listLength <- length(organismList[[1]])
+        searchTerm <- ""
+        searchResult <- 0
+        for(organism in organismList[[1]]){
+            for(code in barcodeList[[1]]){
+                searchTerm <- paste(organism, "[ORGN] AND ", code, "[GENE]", sep="")
+                searchResult <- entrez_search(db = "nucleotide", term = searchTerm)
             }
-            # Define new input
-            newInput <- textInput(newInputId, newInputLabel)
-            # Append new input to list of existing inputs
-            inputTagList <<- tagAppendChild(inputTagList, newInput)
-        })
-        # Return updated list of inputs
-        inputTagList
+        }
+        numResults <- length(searchResult$ids)
+        toString(numResults)
     })
     
 })
