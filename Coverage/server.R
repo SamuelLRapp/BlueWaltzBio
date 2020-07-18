27e5c60dad1f9652c6338771f2447124c1dfcd2d
Sriram Ramesh <sramesh5@ucsc.edu>
2020-07-17 21:37:02 -0700

    Make server.R capable of parsing multi-line input

--- Coverage/server.R
+++ Coverage/server.R
@@ -8,33 +8,15 @@
 #
 
 library(shiny)
+library(rentrez)
 
 shinyServer(function(input, output) {
     
-    output$allInputs <- renderUI({
-        # Get value of button, which represents number of times pressed (i.e. number of inputs added)
-        inputsToShow <- input$appendInput
-        # Return if button not pressed yet
-        if(is.null(inputsToShow) || inputsToShow < 1) return()
-        # Initialize list of inputs
-        inputTagList <- tagList()
-        # Populate the list of inputs
-        lapply(1:inputsToShow,function(i){
-            # Define unique input id and label
-            newInputId <- paste0("input", i)
-            newInputLabel <- paste("Input", i)
-            # Prevent dynamic inputs from resetting
-            newInputValue <- "Option 1"
-            if (newInputId %in% names(input)) {
-                newInputValue <- input[[newInputId]]
-            }
-            # Define new input
-            newInput <- textInput(newInputId, newInputLabel)
-            # Append new input to list of existing inputs
-            inputTagList <<- tagAppendChild(inputTagList, newInput)
-        })
-        # Return updated list of inputs
-        inputTagList
+    
+    
+    output$coverageResults <- renderUI({
+        organismList <- strsplit(input$organismList, ",")[[1]][1]
+        
     })
     
 })
