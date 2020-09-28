7ac68e9a23c6a89530df6917704383a6ff5722cc
Sriram Ramesh <sramesh5@ucsc.edu>
2020-09-28 14:54:09 -0700

    Allow users to search public CRUX databases (#10)

--- Coverage/server.R
+++ Coverage/server.R
@@ -10,13 +10,106 @@
 library(shiny)
 library(rentrez)
 library(taxize)
+library(tidyverse)
+library(dplyr)
 
 shinyServer(function(input, output) {
     
-    coverage <- reactive({
+    file_to_DF <- function(filepath) {                                  #LEAVE THESE HERE FOR LATER
+        taxonomytable<-read.delim(filepath, header=FALSE)
+        taxonomy_only_table <-select(taxonomytable, V2) %>%
+            separate(V2, c("domain", "phylum", "class", "order", "family", "genus", "genus species"), sep = ";",remove=FALSE)
+    }
+    
+    df_18S <- file_to_DF("18S_taxonomy.txt")
+    df_16S <- file_to_DF("16S_taxonomy.txt")
+    df_PITS <- file_to_DF("PITS_taxonomy.txt")
+    df_CO1 <- file_to_DF("CO1_taxonomy.txt")
+    df_FITS <- file_to_DF("FITS_taxonomy.txt")
+    
+    
+    dbList <- list(df_18S, df_16S, df_PITS, df_CO1, df_FITS)
+    
+    
+    cruxCoverage <- reactive({
+        organismList <- cruxOrganismList()
+        organismListLength <- length(organismList)
         
-        organismList <- organismList()
-        organismListLength <- length(organismList())
+        validate(
+            need(organismListLength > 0, 'Please name at least one organism')
+        )
+        searchTerm <- ""
+        searchResult <- 0
+        results <- c()
+        for(organism in organismList){
+            for(table in dbList){
+                location <- which(table == organism, arr.ind = TRUE)
+                if(nrow(location) == 0){
+                    searchTerm <- tax_name(query= organism, get= "genus", db= "ncbi")[1,3]
+                    location <- which(table == searchTerm, arr.ind = TRUE)
+                    if(nrow(location) == 0){
+                        searchTerm <- tax_name(query= organism, get= "family", db= "ncbi")[1,3]
+                        location <- which(table == searchTerm, arr.ind = TRUE)
+                        if(nrow(location)==0){
+                            searchTerm <- tax_name(query= organism, get= "order", db= "ncbi")[1,3]
+                            location <- which(table == searchTerm, arr.ind = TRUE)
+                            if(nrow(location) ==0){
+                                searchTerm <- tax_name(query= organism, get= "class", db= "ncbi")[1,3]
+                                location <- which(table == searchTerm, arr.ind = TRUE)
+                                if(nrow(location)==0){
+                                    searchTerm <- tax_name(query= organism, get= "phylum", db= "ncbi")[1,3]
+                                    location <- which(table == searchTerm, arr.ind = TRUE)
+                                    results <- c(results, nrow(location))
+                                } else { results <- c(results, "class")}
+                            } else {results <- c(results, "order")}
+                        } else {results <- c(results, "family")}
+                    }else {results <- c(results, "genus") }
+                } else {results <- c(results, toString(nrow(location)))}
+            }
+            
+        }
+        data <- matrix(results, nrow = organismListLength, ncol = length(dbList), byrow = TRUE)
+        data
+    })
+    
+    cruxOrgSearch <- eventReactive(input$searchButton, {
+        input$CRUXorganismList
+    })
+    
+    cruxOrganismList <- reactive({
+        organismList <- strsplit(cruxOrgSearch(), ",")[[1]]
+        if(input$CRUXtaxizeOption){
+            taxize_organism_list <- c()
+            
+            for(i in 1:length(organismList))
+            {
+                organism <- trimws(organismList[[i]], "b")
+                NCBI_names <- gnr_resolve(sci = organism, data_source_ids = 4) #4 = NCBI      THIS NEEDS TO GET VERIFIED
+                row_count <- nrow(NCBI_names)
+                
+                if(row_count > 0)
+                {
+                    for(j in 1:row_count)
+                    {
+                        taxa_name <- NCBI_names[[j,3]]
+                        taxize_organism_list <- c(taxize_organism_list, taxa_name)
+                    }
+                }
+                else
+                {
+                    taxize_organism_list <- c(taxize_organism_list, organism)
+                }
+            }
+            taxize_organism_list  
+        } else{
+            organismList
+        }
+    })
+    
+    genBankCoverage <- reactive({
+        
+        organismList <- NCBIorganismList()
+        organismListLength <- length(organismList)
         
         codeListLength <- length(barcodeList())
         validate(
@@ -52,9 +145,10 @@ shinyServer(function(input, output) {
     
     output$seqLenInputs <- renderUI(seqLenList())
 
-    organismList <- reactive({
-        organismList <- strsplit(input$organismList, ",")[[1]]
-        if(input$taxizeOption){
+
+    NCBIorganismList <- reactive({
+        organismList <- strsplit(input$NCBIorganismList, ",")[[1]]
+        if(input$NCBItaxizeOption){
             taxize_organism_list <- c()
             
             for(i in 1:length(organismList))
@@ -87,24 +181,12 @@ shinyServer(function(input, output) {
         barcodeList[[1]]
     })
     
-    output$coverageResults <- DT::renderDataTable(
-        coverage(), rownames = organismList(), colnames = barcodeList()
+    output$NCBIcoverageResults <- DT::renderDataTable(
+        genBankCoverage(), rownames = NCBIorganismList(), colnames = barcodeList()
     )
     
-    output$debug <- renderText(
-        # class(taxize_org_list)
-        # class(barcodeList)
-         #typeof(barcodeList())
-         # typeof(taxize_org_list())
-        #"  "
-         #typeof((gnr_resolve(sci = "homo", data_source_ids = 4)[[1,3]])) 
-        #gnr_resolve(sci = "homo", data_source_ids = 4)[[1,3]]
-        # NCBI_name <- gnr_resolve(sci = "homo", data_source_ids = 4) #4 = NCBI
-        # typeof(strsplit(input$organismList, ",")[[1]])
-         # taxize_org_list()[[2]]
-        # taxize_org_list()[[3]]
-        #length(strsplit(input$organismList, ",")[[1]])
-        organismList()
+    output$CRUXcoverageResults <- DT::renderDataTable(
+        cruxCoverage(), rownames = cruxOrganismList(), colnames = c("18S", "16S", "PITS", "CO1", "FITS")
         
     )
     
