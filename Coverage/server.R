ea58b8d738115f9f7c7e0af954c46619ec09a57d
Sriram Ramesh <sramesh5@ucsc.edu>
2020-11-07 13:38:45 -0800

    Re-organize functions in server.R

--- Coverage/server.R
+++ Coverage/server.R
@@ -16,6 +16,40 @@ library(RSQLite)
 
 shinyServer(function(input, output) {
     
+    cruxOrgSearch <- eventReactive(input$searchButton, {
+        input$CRUXorganismList
+    })
+    
+    cruxOrganismList <- reactive({
+        organismList <- strsplit(cruxOrgSearch(), ",")[[1]]
+        if(input$CRUXtaxizeOption){
+            taxize_organism_list <- c()
+            
+            for(i in 1:length(organismList))
+            {
+                organism <- trimws(organismList[[i]], "b")
+                NCBI_names <- gnr_resolve(sci = organism, data_source_ids = 4) #4 = NCBI      THIS NEEDS TO GET VERIFIED
+                row_count <- nrow(NCBI_names)
+                
+                if(row_count > 0)
+                {
+                    for(j in 1:row_count)
+                    {
+                        taxa_name <- NCBI_names[[j,3]]
+                        taxize_organism_list <- c(taxize_organism_list, taxa_name)
+                    }
+                }
+                else
+                {
+                    taxize_organism_list <- c(taxize_organism_list, organism)
+                }
+            }
+            taxize_organism_list  
+        } else{
+            organismList
+        }
+    })
+    
     cruxCoverage <- reactive({
         organismList <- cruxOrganismList()
         organismListLength <- length(organismList)
@@ -64,80 +98,6 @@ shinyServer(function(input, output) {
         data
     })
     
-    cruxOrgSearch <- eventReactive(input$searchButton, {
-        input$CRUXorganismList
-    })
-    
-    cruxOrganismList <- reactive({
-        organismList <- strsplit(cruxOrgSearch(), ",")[[1]]
-        if(input$CRUXtaxizeOption){
-            taxize_organism_list <- c()
-            
-            for(i in 1:length(organismList))
-            {
-                organism <- trimws(organismList[[i]], "b")
-                NCBI_names <- gnr_resolve(sci = organism, data_source_ids = 4) #4 = NCBI      THIS NEEDS TO GET VERIFIED
-                row_count <- nrow(NCBI_names)
-                
-                if(row_count > 0)
-                {
-                    for(j in 1:row_count)
-                    {
-                        taxa_name <- NCBI_names[[j,3]]
-                        taxize_organism_list <- c(taxize_organism_list, taxa_name)
-                    }
-                }
-                else
-                {
-                    taxize_organism_list <- c(taxize_organism_list, organism)
-                }
-            }
-            taxize_organism_list  
-        } else{
-            organismList
-        }
-    })
-    
-    genBankCoverage <- reactive({
-        
-        organismList <- NCBIorganismList()
-        organismListLength <- length(organismList)
-        
-        codeListLength <- length(barcodeList())
-        validate(
-            need(organismListLength > 0, 'Please name at least one organism'),
-            need(codeListLength > 0, 'Please choose at least one barcode')
-        )
-        searchTerm <- ""
-        searchResult <- 0
-        results <- c()
-        for(organism in organismList){
-            for(code in barcodeList()){
-                searchTerm <- paste(organism, "[ORGN] AND ", code, "[GENE]", sep="")
-                if(input$seqLengthOption){
-                    searchTerm <- paste(searchTerm, " AND ", input[[code]],":99999999[SLEN]", sep="")
-                }
-                searchResult <- entrez_search(db = "nucleotide", term = searchTerm, retmax = 0)$count
-                results <- c(results, searchResult)
-            }
-        }
-        data <- matrix(results, nrow = organismListLength, ncol = codeListLength, byrow = TRUE)
-        data
-    })
-    
-    seqLenList <- reactive({
-        if(input$seqLengthOption){
-            textList <- list()
-            for(marker in barcodeList()){
-                textList <- list(textList, numericInput(marker, paste("Minimum sequence length for", marker), 500, min= 0))
-            }
-            textList
-        }
-    })
-    
-    output$seqLenInputs <- renderUI(seqLenList())
-
-
     NCBIorganismList <- reactive({
         organismList <- strsplit(input$NCBIorganismList, ",")[[1]]
         if(input$NCBItaxizeOption){
@@ -172,6 +132,46 @@ shinyServer(function(input, output) {
         barcodeList <- strsplit(input$barcodeList, ",")
         barcodeList[[1]]
     })
+    
+    seqLenList <- reactive({
+        if(input$seqLengthOption){
+            textList <- list()
+            for(marker in barcodeList()){
+                textList <- list(textList, numericInput(marker, paste("Minimum sequence length for", marker), 500, min= 0))
+            }
+            textList
+        }
+    })
+    
+    genBankCoverage <- reactive({
+        
+        organismList <- NCBIorganismList()
+        organismListLength <- length(organismList)
+        
+        codeListLength <- length(barcodeList())
+        validate(
+            need(organismListLength > 0, 'Please name at least one organism'),
+            need(codeListLength > 0, 'Please choose at least one barcode')
+        )
+        searchTerm <- ""
+        searchResult <- 0
+        results <- c()
+        for(organism in organismList){
+            for(code in barcodeList()){
+                searchTerm <- paste(organism, "[ORGN] AND ", code, "[GENE]", sep="")
+                if(input$seqLengthOption){
+                    searchTerm <- paste(searchTerm, " AND ", input[[code]],":99999999[SLEN]", sep="")
+                }
+                searchResult <- entrez_search(db = "nucleotide", term = searchTerm, retmax = 0)$count
+                results <- c(results, searchResult)
+            }
+        }
+        data <- matrix(results, nrow = organismListLength, ncol = codeListLength, byrow = TRUE)
+        data
+    })
+    
+
+    output$seqLenInputs <- renderUI(seqLenList())
     
     output$NCBIcoverageResults <- DT::renderDataTable(
         genBankCoverage(), rownames = NCBIorganismList(), colnames = barcodeList()
