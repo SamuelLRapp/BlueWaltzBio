16642872a1c78b5d2c11792f2761102b3cb4e18a
Sriram Ramesh <sramesh5@ucsc.edu>
2020-12-24 20:11:07 -0800

    Add comments to our code (#16)
    Co-authored-by: Jorge Tapias Gomez <jtapiasg@ucsc.edu>

--- Coverage/server.R
+++ Coverage/server.R
@@ -7,6 +7,8 @@
 #    http://shiny.rstudio.com/
 #
 
+#;lkasdjf;aldf
+
 library(shiny)
 library(rentrez)
 library(taxize)
@@ -15,6 +17,8 @@ library(dplyr)
 library(RSQLite)
 
 shinyServer(function(input, output) {
+    
+    #CRUX: 
     
     cruxOrgSearch <- eventReactive(input$searchButton, { #When searchButton clicked, update CruxOrgSearch to return the value input into CRUXorganismList 
         input$CRUXorganismList #Returns as a string
@@ -100,77 +104,83 @@ shinyServer(function(input, output) {
         data #return data matrix
     })
     
-    NCBIorganismList <- reactive({
-        organismList <- strsplit(input$NCBIorganismList, ",")[[1]]
-        if(input$NCBItaxizeOption){
-            taxize_organism_list <- c()
+    
+    #NCBI: 
+    
+    NCBIorganismList <- reactive({ #Converts string from NCBIorganismList into a list of Strings
+        organismList <- strsplit(input$NCBIorganismList, ",")[[1]] #separate based on commas
+        if(input$NCBItaxizeOption){ #if the taxize option is selected
+            taxize_organism_list <- c() #initialize an empty vector
             
             for(i in 1:length(organismList))
             {
-                organism <- trimws(organismList[[i]], "b")
+                organism <- trimws(organismList[[i]], "b") #trim both leading and trailing whitespace
                 NCBI_names <- gnr_resolve(sci = organism, data_source_ids = 4) #4 = NCBI
-                row_count <- nrow(NCBI_names)
+                row_count <- nrow(NCBI_names)# get number of rows in dataframe
                 
-                if(row_count > 0)
+                if(row_count > 0)#If a legitimate name was found
                 {
                     for(j in 1:row_count)
                     {
-                        taxa_name <- NCBI_names[[j,3]]
-                        taxize_organism_list <- c(taxize_organism_list, taxa_name)
+                        taxa_name <- NCBI_names[[j,3]] #Store each matched name in taxa_name
+                        taxize_organism_list <- c(taxize_organism_list, taxa_name) #update the vector with all the taxa_names.
                     }
                 }
                 else
                 {
-                    taxize_organism_list <- c(taxize_organism_list, organism)
+                    taxize_organism_list <- c(taxize_organism_list, organism) #just append organism to the list, and return taxize_organism_list
                 }
             }
             taxize_organism_list  
         } else{
-            organismList
+            organismList #return the list as is
         }
     })
     
     barcodeList <- reactive({
-        barcodeList <- strsplit(input$barcodeList, ",")
+        barcodeList <- strsplit(input$barcodeList, ",") #separate based on comma
         barcodeList[[1]]
     })
     
-    seqLenList <- reactive({
-        if(input$seqLengthOption){
+    seqLenList <- reactive({ #list of sequence length specifications
+        if(input$seqLengthOption){ #only present if the option is selected
             textList <- list()
-            for(marker in barcodeList()){
-                textList <- list(textList, numericInput(marker, paste("Minimum sequence length for", marker), 500, min= 0))
+            for(marker in barcodeList()){ #allow the user to specify a different length for every barcode
+                textList <- list(textList, numericInput(marker, paste("Minimum sequence length for", marker), 500, min= 0)) #add a numeric input
             }
-            textList
+            textList #return the list of numeric inputs
         }
     })
     
     genBankCoverage <- reactive({
         
-        organismList <- NCBIorganismList()
+        organismList <- NCBIorganismList() #get species and barcode inputs
         organismListLength <- length(organismList)
         
-        codeListLength <- length(barcodeList())
-        validate(
+        codeListLength <- length(barcodeList()) 
+        validate( #verify that the  user has typed things into both inputs
             need(organismListLength > 0, 'Please name at least one organism'),
             need(codeListLength > 0, 'Please choose at least one barcode')
         )
         searchTerm <- ""
         searchResult <- 0
-        results <- c()
+        results <- c() #initialize empty vector
         for(organism in organismList){
             for(code in barcodeList()){
-                searchTerm <- paste(organism, "[ORGN] AND ", code, "[GENE]", sep="")
+                searchTerm <- paste(organism, "[ORGN] AND ", code, "[GENE]", sep="") #our query to GenBank
                 if(input$seqLengthOption){
-                    searchTerm <- paste(searchTerm, " AND ", input[[code]],":99999999[SLEN]", sep="")
+                    searchTerm <- paste(searchTerm, " AND ", input[[code]],":99999999[SLEN]", sep="") #if the user specified sequence length
                 }
-                searchResult <- entrez_search(db = "nucleotide", term = searchTerm, retmax = 0)$count
-                results <- c(results, searchResult)
+                searchResult <- entrez_search(db = "nucleotide", term = searchTerm, retmax = 0)$count #only get back the number of search results
+                results <- c(results, searchResult) #append the count to the vector of results
             }
         }
-        data <- matrix(results, nrow = organismListLength, ncol = codeListLength, byrow = TRUE)
+        data <- matrix(results, nrow = organismListLength, ncol = codeListLength, byrow = TRUE) #convert results vector to dataframe
         data
     })
+    
+    
+    #outputs:
     
 
     output$seqLenInputs <- renderUI(seqLenList())
