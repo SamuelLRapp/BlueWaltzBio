4ed316f9fcceafd54514f0f273d5df42c07bda87
Jorge Tapias Gomez <59773572+Jotanator@users.noreply.github.com>
2021-01-21 13:16:40 -0800

    Added option to choose whether or not to search by Metadata (#21)
    authored-by: Jorge Tapias Gomez <jtapiasg@ucsc.edu>

--- Coverage/server.R
+++ Coverage/server.R
@@ -180,7 +180,18 @@ shinyServer(function(input, output) {
         results <- c() #initialize empty vector
         for(organism in organismList){
             for(code in barcodeList()){
-                searchTerm <- paste(organism, "[ORGN] AND ", code, "[GENE]", sep="") #our query to GenBank
+                if(input$NCBISearchOptionOrgn){
+                    searchTerm <- paste(organism, "[ORGN] AND ", sep="") #our query to GenBank
+                }
+                else {
+                    searchTerm <- paste(organism, " AND ", sep="") #our non-Metadata query to GenBank
+                }
+                if(input$NCBISearchOptionGene) {
+                    searchTerm <- paste(searchTerm, code, "[GENE]", sep="") #our query to GenBank
+                }
+                else {
+                    searchTerm <- paste(searchTerm, code, sep="") #our query to GenBank
+                }
                 if(input$seqLengthOption){
                     searchTerm <- paste(searchTerm, " AND ", input[[code]],":99999999[SLEN]", sep="") #if the user specified sequence length
                 }
