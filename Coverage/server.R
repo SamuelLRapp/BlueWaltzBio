7051371d0d66960b6703dadd7c6ef0bc855b123f
Sriram Ramesh <sramesh5@ucsc.edu>
2021-03-30 21:22:22 -0700

    Fix CRUX search error

--- Coverage/server.R
+++ Coverage/server.R
@@ -110,13 +110,14 @@ shinyServer(function(input, output, session) {
                   } else {results <- c(results, toString(nrow(location)))}
               }
           }
-        })
+        
             
         dbDisconnect(taxaDB)
         # unlink("taxa-db.sqlite")
         
         data <- matrix(results, nrow = organismListLength, ncol = length(dbList), byrow = TRUE) #store vector results in data matrix
         data #return data matrix
+        })
     })
     
 
