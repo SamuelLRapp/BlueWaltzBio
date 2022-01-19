7498ea005b943fe84d25fc0451e3b7314c98841a
Siram-Ramesh <sramesh5@ucsc.edu>
2022-01-18 19:53:23 -0800

    Added some initial unittests to test_taxize.R

--- Coverage/tests/test_taxize.R
+++ Coverage/tests/test_taxize.R
@@ -3,22 +3,53 @@ library(modules)
 
 orgListHelper <- modules::use("orgListHelper.R")
 
+# The list of fundamental cases (not combined):
+# 1) We toggle the second argument on or off
+# 2) We can have duplicate inputs
+# 3) We can have "odd" characters in the string
+# 4) double commas
+# 5) leading or trailing whitespace
+# 6) double spaces inbetween words in the same org name
+# 7) 
+
 test_that("boolean_off", {
   taxizeHelperOutput <- orgListHelper$taxizeHelper("Homo saapiens", FALSE)
+  expectedOutput <- c("Homo saapiens")
+  expect_equal(taxizeHelperOutput, expectedOutput)
 })
 
 test_that("empty_string", {
   taxizeHelperOutput <- orgListHelper$taxizeHelper("", TRUE)
+  expectedOutput <- c("") #                                                  VERIFY THIS PLEASE 
+  expect_equal(taxizeHelperOutput, expectedOutput)
 })
 
-test_that("empty_string", {
-  taxizeHelperOutput <- orgListHelper$taxizeHelper("", TRUE)
+test_that("simple_test", {
+  taxizeHelperOutput <- orgListHelper$taxizeHelper("Homo saapiens", TRUE)
+  expectedOutput <- c("Homo saapiens", "Homo sapiens", "Homo sapiens subsp. Denisova")
+  expect_equal(taxizeHelperOutput, expectedOutput)
 })
 
-test_that("empty_string", {
-  taxizeHelperOutput <- orgListHelper$taxizeHelper("", TRUE)
+test_that("duplicates_bool_on", {
+  taxizeHelperOutput <- orgListHelper$taxizeHelper("Gallus gallus, Gallus gallus", TRUE)
+  expectedOutput <- c("Gallus gallus")
+  expect_equal(taxizeHelperOutput, expectedOutput)
 })
 
-test_that("empty_string", {
-  taxizeHelperOutput <- orgListHelper$taxizeHelper("", TRUE)
+test_that("duplicates_bool_off", {
+  taxizeHelperOutput <- orgListHelper$taxizeHelper("Homo saapiens, Homo saapiens", FALSE)
+  expectedOutput <- c("Homo saapiens")
+  expect_equal(taxizeHelperOutput, expectedOutput)
+})
+
+test_that("double_comma_bool_on", {
+  taxizeHelperOutput <- orgListHelper$taxizeHelper("Homo saapiens,, Gallus gallus", TRUE)
+  expectedOutput <- c("Homo saapiens", "Homo sapiens", "Homo sapiens subsp. Denisova", "Gallus gallus")
+  expect_equal(taxizeHelperOutput, expectedOutput)
+})
+
+test_that("double_comma_bool_off", {
+  taxizeHelperOutput <- orgListHelper$taxizeHelper("Homo saapiens,, Gallus gallus", TRUE)
+  expectedOutput <- c("Homo saapiens", "Gallus gallus")
+  expect_equal(taxizeHelperOutput, expectedOutput)
 })
