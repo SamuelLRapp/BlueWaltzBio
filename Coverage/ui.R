9f1efb6019301a26d1eff1f0944dbd48f5437435
Zia Truong <zt-home@college9-100-64-55-187.resnet.ucsc.edu>
2022-02-19 17:16:21 -0800

    adding icon to link

--- Coverage/ui.R
+++ Coverage/ui.R
@@ -168,8 +168,11 @@ shinyUI(fluidPage(
                 p("The ‘NCBI Nucleotide Coverage Matrix’ was designed to screen the Nucleotide database for genetic barcode coverage prior to environmental DNA metabarcoding studies. Before conducting a metabarcoding study, scientists need to be aware of which organisms have reference sequences at known genetic barcoding loci. The tool finds out if the Nucleotide database contains sequences labeled with a specific gene and organism name. Numerous searches can be done in parallel instead of manually searching for each organism-gene combination on the NCBI Nucleotide website."),
                 p("The ‘NCBI Nucleotide Coverage Matrix’ tool takes in a list of organisms and genes of interest and then queries the Nucleotide database to find how many records match the search. The tool then produces a table where the organism names are rows, gene names are columns, and each intersection of a row and column shows how many records are in the NCBI Nucleotide database. All of the search options are detailed in the ‘Search fields' section below. The power and flexibility of this tool allows scientists to check the NCBI Nucleotide database for genetic coverage in ways that aren’t possible without knowledge of the NCBI Entrez coding package."),
                 
+                #User Guide: Added by Zia Truong Feb 19 2022
                 h4("User Guide"),
-                tags$a(href="https://docs.google.com/document/d/1-VbO7nzPHY27xDZ714Kzu8xemG2Hsg-Gu5iE8-Da5C8/edit#heading=h.zd4rqn9aw2h","Click this link to read the user guide"),
+                tags$a(href="https://docs.google.com/document/d/1-VbO7nzPHY27xDZ714Kzu8xemG2Hsg-Gu5iE8-Da5C8/edit#heading=h.zd4rqn9aw2h",
+                       icon("question-circle"), #icon from fa v4, please change to circle-question if you update fa version to latest
+                       "Click this link to read the user guide"),
                 
                 h4("Limitations:"),
                 p("This tool may not find all possible entries that the user desires. Some limitations of this text based search include, but are not limited to:"),
