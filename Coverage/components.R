bdc0bfed42b0b99e39677ef40702b1bb794b6f8b
Daniel Wilby <dywilby@gmail.com>
2022-03-15 15:36:17 -0700

    changed info icon return statement

--- Coverage/components.R
+++ Coverage/components.R
@@ -3,10 +3,8 @@ import(shiny)
 
 # pass in the text that you want the tooltip to display
 infoIcon = function(text) {
-  return (
-    tags$div(class = "info-tooltip",
-      tags$span(class = "info-icon", "?"),
-      tags$span(class = "info-tooltip-text", text)         
-    )
-  );
+  tags$div(class = "info-tooltip",
+    tags$span(class = "info-icon", "?"),
+    tags$span(class = "info-tooltip-text", text)         
+  )
 }
