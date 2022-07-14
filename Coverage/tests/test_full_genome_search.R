7e052c9435e3e56da5e829c923f8efae7272ef77
Sriram Ramesh <sramesh5@ucsc.edu>
2022-07-14 14:40:50 -0700

    Refactor server.R functions for the purposes of unit tests (#67)
    * Added checks to handle Phylum and Kingdom in CRUX search
    
    * Fix incorrect if-statement logic on phylum and kingdom case
    
    * Fix some comments/prints to be more descriptive
    
    * Move search term generation for CRUX homonyms to different file
    
    * Revert "Move search term generation for CRUX homonyms to different file"
    
    This reverts commit a56b7b0d2032c83079bc40d26e719d3ee4e05c62.
    
    * initial creation of orgListHelper.R
    
    * Taxize helper function takes in args
    
    * Initial creation of taxize_tests unittests
    
    * Added some initial unittests to test_taxize.R
    
    * commenting out a bunch of stuff
    
    * Fix lingering issue from merge conflict
    
    * Remove deprecated useShinyAlerts() call in ui.R
    
    * factored out some CSV parsing, created server_functions.R
    
    * Fixed the summaryReport bug (#60)
    
    * refactored full genome search functions
    
    * New structure for test_taxize.R, doing away with testthat library
    
    * Fix new test_taxize template
    
    * Added more tests to test_taxize.R
    
    * Fix deduplicating bug in orgListHelper.R
    
    * Reformat taxize helper code
    
    * Fix error in one of the taxize unittests
    
    * Fix issue in emptyString test for taxize code
    
    * update server.R
    
    complete Full Genome Search factoring, CruxCoverage factoring
    
    * updated server_functions.R
    
    added functions associated with the refactoring of server.R
    
    * Update server_functions.R to be imported as a module
    
    * Crux Code upload
    
    uploading because git is refusing connection, and I don't want to deal with that right now.
    
    * NCBIorganismList() refactored
    
    * seqLenList refactor
    
    * updated barcode list function to not need reactive input
    
    * Revert "updated barcode list function to not need reactive input"
    
    This reverts commit aae90ec4bfe4da57b439dba511d3ab5c1b0b3761.
    
    * ncbisearch, genbankcoverage refactor
    
    * Remove duplicate orgList parsing code
    
    * added comment to seqLenList function
    
    * Fix error where deleted function was still being called
    
    * Fix downloading error in full genome tab
    
    * Add initial test file for fullGenomeSearch
    
    * Tidy up server_functions.R
    
    * moved non reactive functions over to server_functions.R
    
    * removed commented out functions
    
    * display table output function and matrix function refactor
    
    * ouput functions refactor (replace old matrixGet())
    
    * unmatched bracket fix
    
    * search terms matrix refactor
    
    * fixed a couple syntax errors. Working now
    
    Co-authored-by: jferre25 <jayferreira2@gmail.com>
    Co-authored-by: Jotanator <59773572+Jotanator@users.noreply.github.com>
    Co-authored-by: jferre25 <52797500+jferre25@users.noreply.github.com>

---
+++ Coverage/tests/test_full_genome_search.R
@@ -0,0 +1,40 @@
+library(modules)
+library(future)
+library(promises)
+library(ipc)
+
+server_functions <- modules::use("Coverage/server_functions.R")
+
+# There are X possible cases
+
+
+function_template <- function(expectedOutput, dbOption, orgList, taxizeOption, refSeqChecked, testName){
+  print("------------------------------------")
+  print("Beginning test ", testName)
+  genSearchOutput <- server_functions$getGenomeSearchFullResults()
+  fileName <- paste("Coverage/tests/test_output/fullGenomeSearchTestOutput/", 
+                    testName, ".csv", sep="")
+  write.table(genSearchOutput, 
+              file=fileName,
+              row.names = FALSE,
+              col.name = FALSE)
+  orgVector <- c()
+  if(file.info(fileName)$size != 0){
+    orgVector <- read.table(fileName)[1]$V1
+  }
+  print(genSearchOutput)
+  print(orgVector)
+  print(expectedOutput)
+  print(paste("results of test ", testName, ":"))
+  print(identical(orgVector, expectedOutput))
+}
+
+# boolean_off ------------------------------------------------------------------
+function_template(c("Homo saapiens", "Homo sapiens"),
+                  "Homo saapiens, Homo sapiens",
+                  FALSE,
+                  "boolean_off")
+
+
+
+
