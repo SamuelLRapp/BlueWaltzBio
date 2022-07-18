4f4c697b070a0e19a6735fc2b77405c0ae083aa3
Siram-Ramesh <sramesh5@ucsc.edu>
2022-07-18 11:25:35 -0700

    add some content to test_full_genome_search.R

--- Coverage/tests/test_full_genome_search.R
+++ Coverage/tests/test_full_genome_search.R
@@ -8,32 +8,35 @@ server_functions <- modules::use("Coverage/server_functions.R")
 # There are X possible cases
 
 
-function_template <- function(expectedOutput, dbOption, orgList, taxizeOption, refSeqChecked, testName){
+function_template <- function(dbOption, orgList, taxizeOption, refSeqChecked, testName){
   print("------------------------------------")
   print("Beginning test ", testName)
-  genSearchOutput <- server_functions$getGenomeSearchFullResults()
+  genSearchOutput <- server_functions$getGenomeSearchFullResults(dbOption, orgList, taxizeOption, refSeqChecked)
   fileName <- paste("Coverage/tests/test_output/fullGenomeSearchTestOutput/", 
                     testName, ".csv", sep="")
+  counts <- genSearchOutput[[1]][[1]]
+  print(counts)
+  orgNames <- genSearchOutput[[2]]
+  uids <- genSearchOutput[[1]][[2]]
+  # for(i in range(nrow()))
   write.table(genSearchOutput, 
               file=fileName,
               row.names = FALSE,
               col.name = FALSE)
-  orgVector <- c()
-  if(file.info(fileName)$size != 0){
-    orgVector <- read.table(fileName)[1]$V1
-  }
-  print(genSearchOutput)
-  print(orgVector)
-  print(expectedOutput)
+  # orgVector <- c()
+  # if(file.info(fileName)$size != 0){
+  #   orgVector <- read.table(fileName)[1]$V1
+  # }
   print(paste("results of test ", testName, ":"))
   print(identical(orgVector, expectedOutput))
 }
 
-# boolean_off ------------------------------------------------------------------
-function_template(c("Homo saapiens", "Homo sapiens"),
-                  "Homo saapiens, Homo sapiens",
+# simple_test_taxize_off -------------------------------------------------------
+function_template("Full mitochondrial genomes in NCBI Nucleotide", 
+                  "Gallus gallus, Canis Lupus",
                   FALSE,
-                  "boolean_off")
+                  TRUE,
+                  "simple_test_taxize_off")
 
 
 
