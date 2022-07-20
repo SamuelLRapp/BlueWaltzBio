bb52057f520e896d5440b3b836cde644b9e3bb00
Siram-Ramesh <sramesh5@ucsc.edu>
2022-07-20 13:29:25 -0700

    Finish writing test template for full genome search

--- Coverage/tests/test_full_genome_search.R
+++ Coverage/tests/test_full_genome_search.R
@@ -2,6 +2,8 @@ library(modules)
 library(future)
 library(promises)
 library(ipc)
+library(rentrez)
+library(taxize)
 
 server_functions <- modules::use("Coverage/server_functions.R")
 
@@ -13,6 +15,7 @@ function_template <- function(dbOption, orgList, taxizeOption, refSeqChecked, te
   genSearchOutput <- server_functions$getGenomeSearchFullResults(dbOption, orgList, taxizeOption, refSeqChecked)
   fileName <- paste("Coverage/tests/test_output/fullGenomeSearchTestOutput/", 
                     testName, ".csv", sep="")
+  close( file( fileName, open="w" ) )
   counts <- genSearchOutput[[1]][[1]][[1]]
   orgNames <- genSearchOutput[[2]]
   uids <- genSearchOutput[[1]][[2]]
@@ -20,11 +23,26 @@ function_template <- function(dbOption, orgList, taxizeOption, refSeqChecked, te
   correct <- TRUE
   for(orgIter in seq.int(1, length(counts))){
     for(i in seq.int(1, counts[[orgIter]])){
+      Sys.sleep(0.34)
       # check if the corresponding uid matches the organism name
       esum <- entrez_summary(db="nucleotide", id=uids[[idIter]])
       # print(names(esum))
-      if(!identical(esum$organism, orgNames[[orgIter]])){
-        write(paste(esum$organism, ", ", orgNames[[orgIter]]),
+      species <- tax_name(query = esum$organism,
+                          get = c("species"),
+                          db = "ncbi",
+                          messages = FALSE)$species
+      Sys.sleep(0.34)
+      
+      foundTaxId <- get_uid_(sci_com=species, messages=FALSE)   # THIS WON'T WORK FOR HOMONYMS CASE
+      foundTaxId <- strtoi(foundTaxId[[species]]$uid, 10)
+      
+      Sys.sleep(0.34)
+      trueTaxId <- get_uid_(sci_com=orgNames[[orgIter]], messages=FALSE)   # THIS WON'T WORK FOR HOMONYMS CASE
+      trueTaxId <- strtoi(trueTaxId[[orgNames[[orgIter]]]]$uid, 10)
+      # print(typeof(trueTaxId))
+      if(!identical(foundTaxId, trueTaxId)){
+        write(paste(esum$organism, ": ", foundTaxId,              # maybe we can change the "found species" printed here
+                    ", ", orgNames[[orgIter]], ": ", trueTaxId),
                     file=fileName, append=TRUE)
         correct <- FALSE
       }
