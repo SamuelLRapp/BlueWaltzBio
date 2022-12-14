473dd2b4fd2bd2a120a971fc43e6c079562c7285
Sriram Ramesh <sramesh5@ucsc.edu>
2022-12-13 20:02:37 -0800

    Add csv files to be used as inputs for testing

--- Coverage/tests/shinytest/FullGenomeCsvUpload.R
+++ Coverage/tests/shinytest/FullGenomeCsvUpload.R
@@ -2,7 +2,7 @@ app <- ShinyDriver$new("../../", seed = 1)
 app$snapshotInit("FullGenomeCsvUpload")
 
 app$snapshot()
-app$uploadFile(uploadGenomeFile = "../LargeCruxSearch(717items).csv") # <-- This should be the path to the file, relative to the app's tests/shinytest directory
+app$uploadFile(uploadGenomeFile = "LargeCruxSearch(717items).csv") # <-- This should be the path to the file, relative to the app's tests/shinytest directory
 app$snapshot()
 app$setInputs(uploadGenomeButton = "click")
 app$snapshot()
